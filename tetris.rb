827867382af5a2c3058f1df893a297335dea2408
Ryosuke1041 <rekyuso11@gmail.com>
2022-07-25 11:26:26 +0900

    color

--- tetris.rb
+++ tetris.rb
@@ -5,15 +5,15 @@ class Tetris
         wall_x = 0
         wall_y = 0
         wallCount = 0
-        sprites1 = []
-        sprites2 = []
-        sprites3 = []
-        sprites4 = []
+        @sprites1 = []
+        @sprites2 = []
+        @sprites3 = []
+        @sprites4 = []
 
         image_wall = Image.load_tiles("./image/59774112_220x220.jpg", 10, 10)
 
         loop do
-            sprites1[wallCount] = Sprite.new(wall_x, 0, image_wall[0])
+            @sprites1[wallCount] = Sprite.new(wall_x, 0, image_wall[0])
             if 220 < wall_x
                 break
             end
@@ -21,7 +21,7 @@ class Tetris
             wallCount = wallCount + 1
         end
         loop do
-            sprites2[wallCount] = Sprite.new(wall_x, 420, image_wall[0])
+            @sprites2[wallCount] = Sprite.new(wall_x, 420, image_wall[0])
             if wall_x < 0
                 break
             end
@@ -29,7 +29,7 @@ class Tetris
             wallCount = wallCount + 1
         end
         loop do
-            sprites3[wallCount] = Sprite.new(0, wall_y, image_wall[0])
+            @sprites3[wallCount] = Sprite.new(-2, wall_y, image_wall[0])
             if 420 < wall_y
                 break
             end
@@ -37,8 +37,8 @@ class Tetris
             wallCount = wallCount + 1
         end
         loop do
-            sprites4[wallCount] = Sprite.new(220, wall_y, image_wall[0])
-            if wall_y < 0
+            @sprites4[wallCount] = Sprite.new(220, wall_y, image_wall[0])
+            if wall_y < -2
                 break
             end
             wall_y = wall_y - 20
@@ -50,30 +50,29 @@ class Tetris
         Window.width = 240
         Window.height = 440
         @blocks = self.createBlocks()
-        print(@blocks)
-        @block_x = 120
-        @block_y = 0
+        print(@blocks[0]["color"])
+        @block_x = 20
+        @block_y = 20
         
         @start = Time.now
         @fallTime = 1.0
-
         #TODO : 秒数経ったら速度を変える
         @speedChangeTime = 60
 
-        img_tohu = Image.new(100,100,blocks.[0].color)
-        tohu = Sprite.new(100,100,img_tohu)
-        @arraydraw = [tohu,sprites1,sprites2,sprites3,sprites4]
+        img_tohu = Image.new(20,20,@blocks[0]["color"])
+        @block = Sprite.new(@block_x,@block_y,img_tohu)
+        @arraydraw = [@block,@sprites1,@sprites2,@sprites3,@sprites4]
 
     end
 
     def createBlocks()
         blocks = [
             {
-                shape: [[[-1, 0], [0, 0], [1, 0], [2, 0]],
+                "shape"=> [[[-1, 0], [0, 0], [1, 0], [2, 0]],
                         [[0, -1], [0, 0], [0, 1], [0, 2]],
                         [[-1, 0], [0, 0], [1, 0], [2, 0]],
                         [[0, -1], [0, 0], [0, 1], [0, 2]]],
-                color: "rgb(0, 255, 255)",
+                "color"=> [255, 0, 255, 255],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(0, 128, 128)"
             },
@@ -82,7 +81,7 @@ class Tetris
                         [[0, 0], [1, 0], [0, 1], [1, 1]],
                         [[0, 0], [1, 0], [0, 1], [1, 1]],
                         [[0, 0], [1, 0], [0, 1], [1, 1]]],
-                color: "rgb(255, 255, 0)",
+                "color"=> [225, 255, 255, 0],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(128, 128, 0)"
             },
@@ -91,7 +90,7 @@ class Tetris
                         [[-1, -1], [-1, 0], [0, 0], [0, 1]],
                         [[0, 0], [1, 0], [-1, 1], [0, 1]],
                         [[-1, -1], [-1, 0], [0, 0], [0, 1]]],
-                color: "rgb(0, 255, 0)",
+                "color"=> [225, 0, 255, 0],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(0, 128, 0)"
             },
@@ -100,7 +99,7 @@ class Tetris
                         [[0, -1], [-1, 0], [0, 0], [-1, 1]],
                         [[-1, 0], [0, 0], [0, 1], [1, 1]],
                         [[0, -1], [-1, 0], [0, 0], [-1, 1]]],
-                color: "rgb(255, 0, 0)",
+                "color"=> [225, 255, 0, 0],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(128, 0, 0)"
             },
@@ -109,7 +108,7 @@ class Tetris
                         [[0, -1], [1, -1], [0, 0], [0, 1]],
                         [[-1, 0], [0, 0], [1, 0], [1, 1]],
                         [[0, -1], [0, 0], [-1, 1], [0, 1]]],
-                color: "rgb(0, 0, 255)",
+                "color"=> [225, 0, 0, 255],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(0, 0, 128)"
             },
@@ -118,7 +117,7 @@ class Tetris
                         [[0, -1], [0, 0], [0, 1], [1, 1]],
                         [[-1, 0], [0, 0], [1, 0], [-1, 1]],
                         [[-1, -1], [0, -1], [0, 0], [0, 1]]],
-                color: "rgb(255, 165, 0)",
+                "color"=> [225, 255, 165, 0],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(128, 82, 0)"
             },
@@ -127,7 +126,7 @@ class Tetris
                         [[0, -1], [0, 0], [1, 0], [0, 1]],
                         [[-1, 0], [0, 0], [1, 0], [0, 1]],
                         [[0, -1], [-1, 0], [0, 0], [0, 1]]],
-                color: "rgb(255, 0, 255)",
+                "color"=> [225, 255, 0, 255],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(128, 0, 128)"
             } 
@@ -148,7 +147,7 @@ class Tetris
         end
     end
 
-
+# mainループからdrewblockを読み込む、drewCellの完成
     def drewCell(type)
         block = blocks[type]
         
@@ -159,21 +158,21 @@ class Tetris
     def mainLoop
         Window.loop do
             if Input.keyDown?(K_RIGHT)
-                block.x += 2
-                if block === box || block === sprites4
-                    block.x -= 2
+                @block.x += 20
+                if @block === @sprites4
+                    @block.x -= 20
                 end
             end
             if Input.keyDown?(K_LEFT)
-                block.x -= 2
-                if block === box || block === sprites3
-                    block.x += 2
+                @block.x -= 20
+                if @block === @sprites3
+                    @block.x += 20
                 end
             end
             if Input.keyDown?(K_DOWN)
-                block.y += 2
-                if block === box || block === sprites2
-                    block.y -= 2
+                @block.y += 20
+                if @block === @sprites2
+                    @block.y -= 20
                 end 
             end
             if Input.keyDown?(K_RETURN)
@@ -188,11 +187,13 @@ class Tetris
                 @speedChangeTime += 60
             end
             if storageTime + @fallTime == newTime
-                block.y -= 20
-                storageTime += startTime + @fallTime
+                @block.y -= 20
+                storageTime += @fallTime
             end
         
             Sprite.draw(@arraydraw)
+
+            sleep 0.06
         end
     end
 
