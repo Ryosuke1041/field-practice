7ba9a4a777dd692892bb637ddda5ef45591fa7c4
Ryosuke1041 <rekyuso11@gmail.com>
2022-07-25 12:03:18 +0900

    wall size

--- tetris.rb
+++ tetris.rb
@@ -10,10 +10,10 @@ class Tetris
         @sprites3 = []
         @sprites4 = []
 
-        image_wall = Image.load_tiles("./image/59774112_220x220.jpg", 10, 10)
-
+        image_wall = Image.new(20, 20,color=[255,192,192,192])
+        
         loop do
-            @sprites1[wallCount] = Sprite.new(wall_x, 0, image_wall[0])
+            @sprites1[wallCount] = Sprite.new(wall_x, 0, image_wall)
             if 220 < wall_x
                 break
             end
@@ -21,7 +21,7 @@ class Tetris
             wallCount = wallCount + 1
         end
         loop do
-            @sprites2[wallCount] = Sprite.new(wall_x, 420, image_wall[0])
+            @sprites2[wallCount] = Sprite.new(wall_x, 420, image_wall)
             if wall_x < 0
                 break
             end
@@ -29,7 +29,7 @@ class Tetris
             wallCount = wallCount + 1
         end
         loop do
-            @sprites3[wallCount] = Sprite.new(-2, wall_y, image_wall[0])
+            @sprites3[wallCount] = Sprite.new(0, wall_y, image_wall)
             if 420 < wall_y
                 break
             end
@@ -37,14 +37,13 @@ class Tetris
             wallCount = wallCount + 1
         end
         loop do
-            @sprites4[wallCount] = Sprite.new(220, wall_y, image_wall[0])
-            if wall_y < -2
+            @sprites4[wallCount] = Sprite.new(220, wall_y, image_wall)
+            if wall_y < 0
                 break
             end
             wall_y = wall_y - 20
             wallCount = wallCount + 1
         end
-
         
 
         Window.width = 240
@@ -81,7 +80,7 @@ class Tetris
                         [[0, 0], [1, 0], [0, 1], [1, 1]],
                         [[0, 0], [1, 0], [0, 1], [1, 1]],
                         [[0, 0], [1, 0], [0, 1], [1, 1]]],
-                "color"=> [225, 255, 255, 0],
+                "color"=> [255, 255, 255, 0],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(128, 128, 0)"
             },
@@ -99,7 +98,7 @@ class Tetris
                         [[0, -1], [-1, 0], [0, 0], [-1, 1]],
                         [[-1, 0], [0, 0], [0, 1], [1, 1]],
                         [[0, -1], [-1, 0], [0, 0], [-1, 1]]],
-                "color"=> [225, 255, 0, 0],
+                "color"=> [255, 255, 0, 0],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(128, 0, 0)"
             },
@@ -108,7 +107,7 @@ class Tetris
                         [[0, -1], [1, -1], [0, 0], [0, 1]],
                         [[-1, 0], [0, 0], [1, 0], [1, 1]],
                         [[0, -1], [0, 0], [-1, 1], [0, 1]]],
-                "color"=> [225, 0, 0, 255],
+                "color"=> [255, 0, 0, 255],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(0, 0, 128)"
             },
@@ -117,7 +116,7 @@ class Tetris
                         [[0, -1], [0, 0], [0, 1], [1, 1]],
                         [[-1, 0], [0, 0], [1, 0], [-1, 1]],
                         [[-1, -1], [0, -1], [0, 0], [0, 1]]],
-                "color"=> [225, 255, 165, 0],
+                "color"=> [255, 255, 165, 0],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(128, 82, 0)"
             },
@@ -126,7 +125,7 @@ class Tetris
                         [[0, -1], [0, 0], [1, 0], [0, 1]],
                         [[-1, 0], [0, 0], [1, 0], [0, 1]],
                         [[0, -1], [-1, 0], [0, 0], [0, 1]]],
-                "color"=> [225, 255, 0, 255],
+                "color"=> [255, 255, 0, 255],
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(128, 0, 128)"
             } 
